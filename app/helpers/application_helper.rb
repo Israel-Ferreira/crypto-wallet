ee9f8c2506a68825dbf448de02f819a4314068d5
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-02-24 23:14:14 +0000

    Aprendendo sobre traduções do ActiveRecord

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-  def data_br(data_us)
-    data_us.strftime("%d/%m/%Y")
+  def attr_name(object, attribute)
+    object.human_attribute_name(attribute)
   end
   
-  def locale(locale)
-    I18n.locale == :en ? "Inglês - EUA" :  "Português do Brasil"
+  def singular_or_plural(object)
+    if object.size > 1
+      object.model_name.human(count: object.size)
+    else
+      object.model_name.human
+    end
   end
   
   def nome_aplicacao
