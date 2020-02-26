4d3dd0c7da3c00334f7ff10c09b30d0561c5c09a
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-02-26 23:20:53 +0000

    Começando a aprender sobre asset pipeline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    #cookies[:curso] = "Curso de Ruby On Rails"
+    #session[:curso] = "Curso de Ruby On Rails"
     @nome = params[:nome] != nil ? params[:nome] : "Fulano"
     @curso = params[:curso]
   end
