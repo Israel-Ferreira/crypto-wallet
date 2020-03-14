bb6f1d69f8e930acf1a41783dca8e04766006710
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-03-14 02:15:16 +0000

    Melhorando o show do mining_type e do coin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def attr_name(object, attribute)
-    object.human_attribute_name(attribute)
+    if defined? object.human_attribute_name(attribute)
+      object.human_attribute_name(attribute)
+    else
+      object.class.human_attribute_name(attribute)
+    end
   end
   
-  def singular_or_plural(object)
-    if object.size > 1
+  def singular_or_plural(object, count=1)
+    if count > 1
       object.model_name.human(count: object.size)
     else
       object.model_name.human
